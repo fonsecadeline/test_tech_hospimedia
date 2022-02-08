20a03734b3c8f7f8ced15f8b96883b4347d57626
fonsecadeline <adeline.fonseca@hotmail.com>
2022-02-08 15:54:08 +0100

    Add helper to clean view and controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,12 @@
 class PagesController < ApplicationController
+	include PagesHelper
+
 	HEAVY_LIMIT = 75
 
 	before_action :set_characters, only: [:heavy_characters]
 
 	def set_characters
-		response = RestClient.get 'https://swapi.dev/api/people/'
-		if response.code == 200
-			@characters = JSON.parse(
-				RestClient.get('https://swapi.dev/api/people/', headers={}), symbolize_names: true)[:results]
-		else
-			# TODO : edit flash to say something did not go as expected and reload page
-			@characters = []
-		end
+		@characters = ask_url('https://swapi.dev/api/people/')[:results]
 	end
 
 	def heavy_characters
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,11 @@
 module PagesHelper
+	def ask_url(url_adress)
+		response = RestClient.get url_adress
+
+
+		# TODO : edit flash to say something did not go as expected and reload page
+		return {} unless response.code == 200
+
+		JSON.parse(response.body, symbolize_names: true)
+	end
 end
