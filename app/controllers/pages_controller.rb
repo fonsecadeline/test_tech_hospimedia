bb5c2061b72f45aa19b465dfbfde212897e791a5
fonsecadeline <adeline.fonseca@hotmail.com>
2022-02-08 11:30:23 +0100

    Output heavy_characters page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,20 @@
 class PagesController < ApplicationController
+	HEAVY_LIMIT = 75
+
+	before_action :set_characters, only: [:heavy_characters]
+
+	def set_characters
+		response = RestClient.get 'https://swapi.dev/api/people/'
+		if response.code == 200
+			@characters = JSON.parse(
+				RestClient.get('https://swapi.dev/api/people/', headers={}), symbolize_names: true)[:results]
+		else
+			# TODO : edit flash to say something did not go as expected and reload page
+			@characters = []
+		end
+	end
+
 	def heavy_characters
+		@heavy_characters = @characters.select{ |char| char[:mass].to_i > 75 }.group_by{ |char| char[:films] }
 	end
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "heavy_characters are heavy" do
+    @characters = (50..80).collect{ |mass| { name: mass.to_s, mass: mass, films: 'movie name' } }
+    # TODO : ensure heavy_characters contains 5 values
+    @characters = (50..60).collect{ |mass| { name: mass.to_s, mass: mass, films: 'movie name' } }
+    # TODO : ensure heavy_characters contains no values
+    @characters = []
+    # TODO : ensure heavy_characters contains no values and no error is thrown
+  end
+
+  test "heavy_characters are grouped_by movie"
+    @characters =
+      ['movie1', 'movie2', 'movie3'].collect.with_index{ |movie_name, index|
+        { name: "name_#{index}", mass: 80, films: movie_name }
+      }
+    # TODO : ensure 3 groups are created, all characters were kept
+
+    @characters.each{ |char| char[:films] = 'same_movie' }
+    # TODO : ensure only 1 group is created, all characters were kept
+  end
 end
